c43d54ea1f6ac2d2e662a463f3a6dd7f5d3d2349
EPAM, Alena Tsiarentsyeva <Alena_Tsiarentsyeva@epam.com>
2021-09-26 00:02:25 +0300

    implement importProductsFile function

--- import-service/src/libs/apiGateway.ts
+++ import-service/src/libs/apiGateway.ts
@@ -1,12 +1,11 @@
-import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from "aws-lambda"
-import type { FromSchema } from "json-schema-to-ts";
-
-type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
-export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
-
-export const formatJSONResponse = (response: Record<string, unknown>) => {
+export const formatResponse = (statusCode: number, result: string) => {
   return {
-    statusCode: 200,
-    body: JSON.stringify(response)
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Origin": "*",
+    },
+    statusCode: statusCode,
+    body: result
   }
 }
